6602d680efb1a0fa5dc1880e1dcd084e07eca8eb
Amr Ayoub <Amr Ayoub>
2016-10-20 18:57:56 +0300

    Add operations to vector class
        plus, minus, scalar multiply
        magnitude,normalize

---
+++ Lin_Alg/src/test.py
@@ -0,0 +1,13 @@
+'''
+Created on Oct 20, 2016
+
+@author: Amr
+'''
+
+import vector.vect
+
+x = vector.vect.Vector([5.581,-2.136])
+y = vector.vect.Vector([1.996,3.108,-4.554])
+
+print(x.normalize())
+print(y.normalize())
--- Lin_Alg/src/vector/vect.py
+++ Lin_Alg/src/vector/vect.py
@@ -1,3 +1,5 @@
+import math
+
 
 class Vector(object):
     def __init__(self, coordinates):
@@ -13,7 +15,60 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
-
+    
+    
+    def plus(self,x):
+        new_coordinates= []
+        n = len(self.coordinates)
+        
+        for i in range(n):
+            new_coordinates.append(self.coordinates[i] + x.coordinates[i])
+            
+        return Vector(new_coordinates)
+            
+            
+    def minus(self,x):
+        new_coordinates = []
+        n=len(self.coordinates)
+        
+        for i in range(n):
+            new_coordinates.append(self.coordinates[i] - x.coordinates[i])
+            
+         
+        return Vector(new_coordinates)   
+                
+    
+    def scalar(self,x):
+        new_coordinates = []
+        n=len(self.coordinates)
+        
+        for i in range(n):
+            new_coordinates.append(self.coordinates[i] * x)
+            
+         
+        return Vector(new_coordinates)
+    
+            
+            
+    def magnitude(self):
+        n=len(self.coordinates)
+        res = 0
+        
+        for i in range(n):
+            res = res + math.pow(self.coordinates[i], 2)
+        
+        res = math.sqrt(res)
+        
+        return res
+    
+    
+    def normalize(self):
+        magnitude = self.magnitude()
+        magnitude = 1 /magnitude
+        
+        return (self.scalar(magnitude))
+        
+        
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
